0a9ca815570cd1416a8607017c0e21b66b28aafe
Mohammed Fasil V <fasilmveloor@gmail.com>
2023-09-24 18:51:56 +0530

    add new schemas for new routes

--- schemas.py
+++ schemas.py
@@ -7,3 +7,24 @@ class UserCreate(BaseModel):
     email: str
     password: str
     is_active: bool
+
+class requestdetails(BaseModel):
+    email:str
+    password:str
+        
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+
+class changepassword(BaseModel):
+    email:str
+    old_password:str
+    new_password:str
+
+class TokenCreate(BaseModel):
+    user_id:str
+    access_token:str
+    refresh_token:str
+    status:bool
+    created_date:datetime.datetime
+    
