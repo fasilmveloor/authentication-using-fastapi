f2d31c73e4bb33ebe707cfa0acf15d06e13d2dcb
Mohammed Fasil V <fasilmveloor@gmail.com>
2023-09-24 18:51:04 +0530

    fix formating

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/dev-db"
+DATABASE_URL = "postgresql://postgres:123@localhost:5432/nest"
 
 engine = create_engine(DATABASE_URL)
 
